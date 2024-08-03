eeb8976aac2394d6192b274a07f9f48d981335f0
GerardoM <you@example.com>
2024-08-02 23:16:40 -0600

    imagen

--- src/main/java/org/example/Auto.java
+++ src/main/java/org/example/Auto.java
@@ -15,50 +15,5 @@ public class Auto {
         this.cantidadDpasajeros = cantidadDpasajeros;
     }
 
-    public String getMarca() {
-        return marca;
-    }
-
-    public void setMarca(String marca) {
-        this.marca = marca;
-    }
-
-    public String getColor() {
-        return color;
-    }
-
-    public void setColor(String color) {
-        this.color = color;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public String getModelo() {
-        return modelo;
-    }
-
-    public void setModelo(String modelo) {
-        this.modelo = modelo;
-    }
-
-    public String getCantidadDpasajeros() {
-        return cantidadDpasajeros;
-    }
-
-    public void setCantidadDpasajeros(String cantidadDpasajeros) {
-        this.cantidadDpasajeros = cantidadDpasajeros;
-    }
-    public void infoAuto(){
-        System.out.println("marca: "+getMarca());
-        System.out.println("color: "+getColor());
-        System.out.println("tipo: "+getTipo());
-        System.out.println("modelo: "+getModelo());
-        System.out.println("cantidad pasajeros: "+getCantidadDpasajeros());
-    }
+    git add .
 }
